1e02aad78f1d282d9e532f24ef2bfd0a3e924af7
Edward King <king8three@gmailcom>
2024-05-18 18:08:53 -0400

    Line 6 Updated

--- how-to-write-code/consolelogexamples01.js
+++ how-to-write-code/consolelogexamples01.js
@@ -3,4 +3,4 @@ console.log(2001);
 console.log("What","do","commas","do?");
 console.log("Does", "adding",      "space", "matter?");
 console.log('Launch' + 'Code');
-console.log("LaunchCode was founded in", 2013);
+console.log("LaunchCode\twas\tfounded\tin",2013);
