4c0a6caf4bd863aafe56e8a3e446365aa49c4f82
Edward King <king8three@gmailcom>
2024-05-19 18:25:28 -0400

    User Input

--- how-to-write-code/consolelogexamples01.js
+++ how-to-write-code/consolelogexamples01.js
@@ -1,6 +1,32 @@
-console.log('Hello, JavaScript.');
+/*console.log('Hello, JavaScript.');
 console.log(2001);
 console.log("What","do","commas","do?");
 console.log("Does", "adding",      "space", "matter?");
 console.log('Launch' + 'Code');
-console.log("LaunchCode\twas\tfounded\tin",2013);
+console.log("LaunchCode\twas\tfounded\tin",2013);*/
+
+/*console.log(typeof "17");
+console.log(typeof "3.2");*/
+
+/*console.log('Bruce's beard');*/
+ //console.log(Number('three'))
+
+ /*let day = "Thursday";
+day = 32.5;
+day = 19;
+console.log(day);*/
+
+//console.log(16 - 2 * 5 / 3 + 1)
+
+//console.log(1 + 5 % 3);
+
+//console.log(2 ** 2 ** 3 * 3)
+
+const input = require('readline-sync');
+
+let name = input.question("Enter your name: ");
+
+let lastName = input.question("Enter your last name: ");
+
+console.log("Hello "+name+" " +lastName+"!");
+
