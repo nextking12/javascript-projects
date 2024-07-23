cf805e9722f3be0094e111fff7640dda05255d99
Edward King <king8three@gmailcom>
2024-07-22 21:34:43 -0400

    Worked on exercise

--- classes/exercises/ClassExercises.js
+++ classes/exercises/ClassExercises.js
@@ -1,9 +1,32 @@
 // Define your Book class here:
+class Book {
+    constructor(title, author, copyrightDate, isbn, numOfPages, timesCheckedOut, discarded = false ){
+        this.title = title;
+        this.author = author;
+        this.copyrightDate = copyrightDate;
+        this.isbn = isbn;
+        this.numOfPages = numOfPages;
+        this.timesCheckedOut = timesCheckedOut;
+        this.discarded = discarded;
 
+
+    }
+};
 
 // Define your Manual and Novel classes here:
+class Manual extends Book {
+    constructor(title, author, copyrightDate, isbn, numOfPages, timesCheckedOut, discarded = false){
+        super(title, author, copyrightDate, isbn, numOfPages, timesCheckedOut, discarded = false)
 
+    }
+};
 
+class Novel{
+    constructor(title, author, copyrightDate, isbn, numOfPages, timesCheckedOut, discarded = false){
+        super(title, author, copyrightDate, isbn, numOfPages, timesCheckedOut, discarded = false)
+
+    }
+};
 // Declare the objects for exercises 2 and 3 here:
 
 
