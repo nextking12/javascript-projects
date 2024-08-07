59667293b76d1bf3215cb41edcd80a4c41b7a324
Edward King <king8three@gmailcom>
2024-08-07 17:22:52 -0400

    worked on studio

--- dom-and-events/studio/scripts.js
+++ dom-and-events/studio/scripts.js
@@ -1,2 +1,30 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function () {
+
+    const takeoffButton = document.getElementById("takeoff");
+
+    const flightStatus = document.getElementById("flightStatus");
+
+    const shuttleBackground = document.getElementById("shuttleBackground");
+
+    const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+
+    takeoffButton.addEventListener("click", function (){
+
+        let takeoffConfirm = confirm("Conform that the shuttle is ready for takeoff");
+
+        if (takeoffConfirm){
+
+            flightStatus.innerHTML = "Shuttle in flight!";
+            shuttleBackground.style.backgroundColor = "blue";
+            spaceShuttleHeight.innerHTML = "10000";
+
+        }
+
+    });
+
+
+
+    
+});
